0aadca3b8fc33a3d9c32f3dd5c3fef2d59c529bc
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-04-26 16:26:06 +0200

    Credentials tests added

--- test/Proposals_Tests.js
+++ test/Proposals_Tests.js
@@ -73,16 +73,16 @@ contract("Testing Proposals",function(accounts){
         }
         catch(error){
             expect(error.message).to.match(NotAllowedToApproveProposals);
-            try{
-                var credentialsAddress = await proposals.retrieveCredentialsContractAddress({from: user_1});
-                var credentials = new web3.eth.Contract(CredentialsAbi, credentialsAddress);
-                var info = await credentials.methods.retrieveProvider(provider_1).call({from: user_1}, function(error, result){});
-                expect.fail();
-            }
-            catch(error){
-                expect(error.message).to.match(ProviderDoesNotExist);
-            } 
         }
+        try{
+            var credentialsAddress = await proposals.retrieveCredentialsContractAddress({from: user_1});
+            var credentials = new web3.eth.Contract(CredentialsAbi, credentialsAddress);
+            var info = await credentials.methods.retrieveProvider(provider_1).call({from: user_1}, function(error, result){});
+            expect.fail();
+        }
+        catch(error){
+            expect(error.message).to.match(ProviderDoesNotExist);
+        } 
     });
 
     it("Approve Proposal CORRECT Chair Person",async function(){
@@ -101,30 +101,93 @@ contract("Testing Proposals",function(accounts){
 contract("Testing Credentials", function(accounts){
     var proposals;
     var credentials;
-    var creator;
     // used addresses
-    const chairPerson = accounts[0];
-    const provider_1 = accounts[1];  
+    const owner_1 = accounts[0]; // owner and chair person
+    const owner_2 = accounts[1];
+    const provider_1 = accounts[2];  
     const user_1 = accounts[2];
     // providers info
     const provider_1_Info = "Account 1 Info";
     // test constants
+    const NotAllowedToAddOwners = new RegExp(/(Not allowed to add owners)/g);
+    const OwnerAlreadyActivated = new RegExp(/(Owner already activated)/g);
+    const NotAllowedToRemoveOwners = new RegExp(/(Not allowed to remove owners)/g);
+    const OwnerAlreadyDeactivated = new RegExp(/(Owner already de-activated)/g);
     const addressesLength = 42;
     const Gas = 600000;
 
     beforeEach(async function(){
-        proposals = await Proposals.new({from: chairPerson});
+        proposals = await Proposals.new({from: owner_1});
         var credentialsAddress = await proposals.retrieveCredentialsContractAddress({from: user_1});
-        credentials = new web3.eth.Contract(CredentialsAbi, credentialsAddress);
+        credentials = new web3.eth.Contract(CredentialsAbi,credentialsAddress);
     });
 
     it("Retrieve Creator",async function(){
        // act
-       creator = await credentials.methods.retrieveCreator().call({from: user_1}, function(error, result){});
-       console.log(" " + creator);
+       let creator = await credentials.methods.retrieveCreator().call({from: user_1}, function(error, result){});
        // assert
-       expect(creator).to.equal(proposals);
+       expect(creator).to.equal(proposals.address);
     });
+
+    it("Add Owners WRONG",async function(){
+        try{
+            await credentials.methods.addOwner(owner_2).send({from: user_1}, function(error, result){});
+            expect.fail();
+        }
+        catch(error){
+            expect(error.message).to.match(NotAllowedToAddOwners);
+        } 
+        try{
+            await credentials.methods.addOwner(owner_1).send({from: owner_1}, function(error, result){});
+            expect.fail();
+        }
+        catch(error){
+            expect(error.message).to.match(OwnerAlreadyActivated);
+        }
+     });
+
+    it("Add Owners CORRECT",async function(){
+        // act
+        await credentials.methods.addOwner(owner_2).send({from: owner_1}, function(error, result){});
+        const TotalOwners = await credentials.methods.retrieveTotalOwners().call({from: user_1}, function(error, result){});
+        let ISOwner1 = await credentials.methods.isOwner(owner_1).call({from: user_1}, function(error, result){});
+        let ISOwner2 = await credentials.methods.isOwner(owner_2).call({from: user_1}, function(error, result){});
+        let ISCreatorOwner = await credentials.methods.isOwner(proposals.address).call({from: user_1}, function(error, result){});
+        // assert
+        expect(parseInt(TotalOwners)).to.equal(3);
+        expect(ISCreatorOwner).to.be.true;
+        expect(ISOwner1).to.be.true;
+        expect(ISOwner2).to.be.true;
+     });
+
+     it("Remove Owners WRONG",async function(){
+        try{
+            await credentials.methods.removeOwner(owner_1).send({from: user_1}, function(error, result){});
+            expect.fail();
+        }
+        catch(error){
+            expect(error.message).to.match(NotAllowedToRemoveOwners);
+        } 
+        try{
+            await credentials.methods.removeOwner(owner_2).send({from: owner_1}, function(error, result){});
+            expect.fail();
+        }
+        catch(error){
+            expect(error.message).to.match(OwnerAlreadyDeactivated);
+        }
+     });
+
+    it("Remove Owners CORRECT",async function(){
+        // act
+        await credentials.methods.removeOwner(owner_1).send({from: owner_1}, function(error, result){});
+        const TotalOwners = await credentials.methods.retrieveTotalOwners().call({from: user_1}, function(error, result){});
+        let ISOwner1 = await credentials.methods.isOwner(owner_1).call({from: user_1}, function(error, result){});
+        let ISCreatorOwner = await credentials.methods.isOwner(proposals.address).call({from: user_1}, function(error, result){});
+        // assert
+        expect(parseInt(TotalOwners)).to.equal(1);
+        expect(ISCreatorOwner).to.be.true;
+        expect(ISOwner1).to.be.false;
+     });
 
     
     
