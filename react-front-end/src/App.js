2492f4500e1760f0895dffac7e5cfdf3c19e4189
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-06-18 11:00:17 +0200

    commit

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import Tabs from "./components/Tabs"; 
-import Admin from "./components/Admin"; 
+import React, { Component } from 'react';
+import Home from "./components/Home"; 
+import NotFound from "./components/NotFound"; 
+import { browserHistory, Router, Route } from 'react-router';
+//import Admin from "./components/Admin"; 
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div>
       <h1>CERTIFICATES</h1>
@@ -20,6 +22,36 @@ function App() {
      </Tabs> 
     </div>
   );
+}*/
+
+class App extends Component {
+  render(){
+    return(
+      <Router history={history}>
+        <Container>
+
+          <Menu secondary>
+            <Menu.Item 
+              name='home'
+              onClick={this.navigateToHome}
+            />
+          </Menu>
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route  component={NotFound} />
+          </Switch>
+
+        </Container>
+      </Router>
+    );
+  }
+
+
+  navigateToHome(e){
+    e.preventDefault();
+    history.push('/');
+  }
 }
 
 export default App;
--- react-front-end/src/components/Admin.js
+++ react-front-end/src/components/Admin.js
@@ -4,20 +4,20 @@ class Admin extends React.Component {
 	render() {
   	return (
         <div>
-            <form action="">
+            <form id="CreatePrivatePoolForm">
                 <input type="text" placeholder="List of owners"/>
                 <input type="integer" placeholder="Min owners"/>
                 <button>Create Private Pool</button>
             </form>
             < br></br>
-            <form action="">
+            <form id="SubmitPublicPoolForm">
                 <input type="text" placeholder="Provider Address"/>
                 <input type="text" placeholder="Provider Info"/>
                 <button>Submit to Public Pool</button>
             </form>
         </div>	
     );
-  }
+  }s
 }
 
 export default Admin;
