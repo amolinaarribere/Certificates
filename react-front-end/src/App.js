997107a6957722244b6cc970d33c162e83271f62
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-06-22 18:27:53 +0200

    React Front End

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,56 +1,86 @@
-import React, { Component } from 'react';
-import Home from "./components/Home"; 
-import NotFound from "./components/NotFound"; 
-import { browserHistory, Router, Route } from 'react-router';
-//import Admin from "./components/Admin"; 
-import './App.css';
-
-/*function App() {
-  return (
-    <div>
-      <h1>CERTIFICATES</h1>
-      <Tabs> 
-       <div id="AdminTab" label="Admin"> 
-         <Admin></Admin> 
-       </div> 
-       <div id="PublicTab" label="Public Pool"> 
-         After 'while, <em>Crocodile</em>! 
-       </div> 
-       <div id="PrivateTab" label="Private Pools"> 
-         Nothing to see here, this tab is <em>extinct</em>! 
-       </div> 
-     </Tabs> 
-    </div>
-  );
-}*/
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
+import TodoList from './TodoList'
 
 class App extends Component {
-  render(){
-    return(
-      <Router history={history}>
-        <Container>
-
-          <Menu secondary>
-            <Menu.Item 
-              name='home'
-              onClick={this.navigateToHome}
-            />
-          </Menu>
-
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route  component={NotFound} />
-          </Switch>
-
-        </Container>
-      </Router>
-    );
+  componentWillMount() {
+    this.loadBlockchainData()
   }
 
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+    this.setState({ todoList })
+    const taskCount = await todoList.methods.taskCount().call()
+    this.setState({ taskCount })
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await todoList.methods.tasks(i).call()
+      this.setState({
+        tasks: [...this.state.tasks, task]
+      })
+    }
+    this.setState({ loading: false })
+  }
 
-  navigateToHome(e){
-    e.preventDefault();
-    history.push('/');
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      taskCount: 0,
+      tasks: [],
+      loading: true
+    }
+
+    this.createTask = this.createTask.bind(this)
+    this.toggleCompleted = this.toggleCompleted.bind(this)
+  }
+
+  createTask(content) {
+    this.setState({ loading: true })
+    this.state.todoList.methods.createTask(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  toggleCompleted(taskId) {
+    this.setState({ loading: true })
+    this.state.todoList.methods.toggleCompleted(taskId).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <TodoList
+                  tasks={this.state.tasks}
+                  createTask={this.createTask}
+                  toggleCompleted={this.toggleCompleted} />
+              }
+            </main>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
