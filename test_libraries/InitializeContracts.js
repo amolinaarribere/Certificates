e217766eb8768dcada841f89fcaba6b4010356a2
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-07-21 16:25:15 +0200

    commit chairperson

--- test_libraries/InitializeContracts.js
+++ test_libraries/InitializeContracts.js
@@ -16,7 +16,7 @@ async function InitializeContracts(chairPerson, PublicOwners, minOwners, user_1,
     let certPoolManager = await CertificatesPoolManager.new({from: chairPerson});
     let certisToken = await CertisToken.new("Certis Token for Test", "CERT", 0, TotalTokenSupply, {from: chairPerson});
     let publicPool = await PublicCertificatesPool.new(PublicOwners, minOwners, certPoolManager.address, {from: user_1});
-    let treasury = await Treasury.new(PublicPriceWei, PrivatePriceWei, CertificatePriceWei, OwnerRefundPriceWei, publicPool.address, certPoolManager.address, certisToken.address, PropositionLifeTime, PropositionThresholdPercentage, minPercentageToPropose, {from: user_1});
+    let treasury = await Treasury.new(PublicPriceWei, PrivatePriceWei, CertificatePriceWei, OwnerRefundPriceWei, publicPool.address, certPoolManager.address, certisToken.address, PropositionLifeTime, PropositionThresholdPercentage, minPercentageToPropose, {from: chairPerson});
     await certPoolManager.Initialize(publicPool.address, treasury.address);
 
     return [certPoolManager, certisToken];
