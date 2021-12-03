e9910cecb19bada05a10859c9632f39a518ce3af
Alberto Molina Arribere <amolina@vaudoise.ch>
2021-12-03 09:54:19 +0100

    added event for Reverse Registry ENS

--- contracts/Base/ReverseRegistryBaseContract.sol
+++ contracts/Base/ReverseRegistryBaseContract.sol
@@ -10,11 +10,15 @@ pragma solidity 0.8.7;
 
  contract ReverseRegistryBaseContract {
 
+    // EVENTS /////////////////////////////////////////
+    event _NewReverseENSCreated(string ENSName, bytes32 node, address addr);
+
     // CONSTRUCTOR & INIT /////////////////////////////////////////
     function RegisterReverseAddress(string memory ENSName, address ReverseRegistryAddress) internal 
     {
         IENSReverseRegistry ReverseRegistry = IENSReverseRegistry(ReverseRegistryAddress);
-        ReverseRegistry.setName(ENSName);
+        bytes32 node = ReverseRegistry.setName(ENSName);
+        emit _NewReverseENSCreated(ENSName, node, address(this));
     }
 
   }
